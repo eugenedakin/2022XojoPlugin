67cf405c82429ddcb865b0b8d3ad7b8264b0cab1
Eugene Dakin <eugenedakin@hotmail.com>
2022-07-21 21:07:38 -0600

    Delete AddTwoNum.h

--- AddTwoNum/AddTwoNum.h
+++
@@ -1,9 +0,0 @@
-#include "WinHeader++.h"
-#include "rb_plugin.h"
-
-int AddTwo(int x, int y);
-
-
-REALmethodDefinition TestModuleMethods[] = {
-	{ (REALproc)AddTwo, REALnoImplementation, "AddTwo(x as integer, y as integer) as integer", REALconsoleSafe },
-};
